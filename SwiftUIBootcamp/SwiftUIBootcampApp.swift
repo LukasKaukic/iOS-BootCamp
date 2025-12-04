ad0570f4d6b2b1f5ce311942e362c25d330ad5bf
Lukáš Kaukič <lukas.kaukic98@gmail.com>
2025-12-04 16:20:47 +0100

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Lukáš Kaukič on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIBootcampTests/SwiftUIBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampTests.swift
+//  SwiftUIBootcampTests
+//
+//  Created by Lukáš Kaukič on 04.12.2025.
+//
+
+import Testing
+@testable import SwiftUIBootcamp
+
+struct SwiftUIBootcampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
