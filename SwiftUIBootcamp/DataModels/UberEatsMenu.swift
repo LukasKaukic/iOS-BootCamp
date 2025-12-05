31b35baacc2a20d2894c328a52f3bc7c0524c3fc
Lukáš Kaukič <lukas.kaukic98@gmail.com>
2025-12-05 16:11:41 +0100

    Adding UberEatsMenu/ViewExtractionModule

---
+++ SwiftUIBootcamp/DataModels/UberEatsMenu.swift
@@ -0,0 +1,18 @@
+//
+//  UberEatsMenu.swift
+//  SwiftUIBootcamp
+//
+//  Created by Lukáš Kaukič on 05.12.2025.
+//
+
+import SwiftUI
+
+struct UberEatsMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UberEatsMenu()
+}
---
+++ SwiftUIBootcamp/ViewExtraction/ViewExtractionModule.swift
@@ -0,0 +1,18 @@
+//
+//  ViewExtractionModule.swift
+//  SwiftUIBootcamp
+//
+//  Created by Lukáš Kaukič on 05.12.2025.
+//
+
+import SwiftUI
+
+struct ViewExtractionModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewExtractionModule()
+}
